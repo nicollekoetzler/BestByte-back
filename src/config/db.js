79a2e4324d6b25775bbb56aaecb90abac5f3eaed
Gabriel <gabriel.liradefarias2@gmail.com>
2022-09-15 11:08:31 -0300

    configurações iniciais

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db;
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db(process.env.DATABASE);
+    
+} catch (error) {
+    console.log(error);
+}
+
+export default db
