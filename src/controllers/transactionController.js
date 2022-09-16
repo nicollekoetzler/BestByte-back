a12bd977a33dbffddff7aa2874f2ac335f1817ea
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-15 22:26:30 -0300

    Funcionalidades com middleware

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,10 +1,22 @@
 import mongo from '../db/db.js';
+import { COLLECTIONS } from '../enums/collections.js';
+import { STATUS_CODE } from '../enums/statusCode.js';
 
 async function insert (req, res) {
-    const token = req.headers.authorization?.replace('Bearer ', '');
 
-    if(!token) {
-        return res.send()
+    const { session, user } = res.locals;
+
+    console.log(session);
+
+    try {
+        mongo.collection(COLLECTIONS).insertOne({
+            userId: sessionStorage.userId,
+        })
+
+        return res.send(STATUS_CODE.CREATED);
+
+    } catch (error) {
+        return res.send(STATUS_CODE.SEVER_ERROR);
     }
 }
 
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { signUp, signIn } from '../controllers/authController.js';
 
-
 const router = express.Router();
 router.post('/sign-up', signUp);
 router.post('/sign-in', signIn);
