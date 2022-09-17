2defe3d5c5a1b7883f43a283298ef7752a21d3eb
Gabriel <gabriel.liradefarias2@gmail.com>
2022-09-17 19:00:26 -0300

    enviando userId para carrinho

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -13,26 +13,25 @@ export async function listarProdutos(req, res){
     }
 }
 
-// export async function enviarCarrinho(req, res){
-//     const carrinho = req.body
-//     const token = req.headers["token"]
-//     try {
-//         const validToken = await db.collection('sessions').findOne({
-//             token: token
-//         }) 
-//         if(!validToken){
-//             return res.sendStatus(401)
-//         }
-//         await db.collection('cart').insertOne({
-//             userId: validToken.userId,
-//             price: carrinho.price,
-//             name: carrinho.name,
-//             image: carrinho.image
-//         })
-//         console.log(userId)
-//         return res.sendStatus(200)
-//     } catch (error) {
-//         console.log(error)
-//         return res.sendStatus(500)
-//     }
-// }
+export async function enviarCarrinho(req, res){
+    const carrinho = req.body
+    const token = req.headers["Authorization"]
+    try {
+        const validToken = await db.collection('sessions').findOne({
+            token: token
+        }) 
+        if(!validToken){
+            return res.sendStatus(401)
+        }
+        await db.collection('cart').insertOne({
+            userId: validToken.userId,
+            price: carrinho.price,
+            name: carrinho.name,
+            image: carrinho.image
+        })
+        return res.sendStatus(200)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
+}
